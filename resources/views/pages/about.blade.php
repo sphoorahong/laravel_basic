00f624e6d5cde9fc8e4c2f09b8500f7f500a98e6
sphoorahong <phoorahong@gmail.com>
2018-07-13 14:15:01 +0700

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit incidunt aut aliquam alias tenetur est natus perspiciatis,
+        deserunt esse possimus minima! Ad praesentium deserunt quas necessitatibus illo eius perspiciatis eos!</p>
+@endsection
